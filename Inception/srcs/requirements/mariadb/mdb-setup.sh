cd790cc140eb9ac39d1f200b7e8c32beb4e04918
Mohamed amine Naimi <mnaimi@student.1337.ma>
2023-03-17 11:10:32 +0100

    Still need to fix some errors in the wordpress configuration

--- Inception/srcs/requirements/mariadb/mdb-setup.sh
+++ Inception/srcs/requirements/mariadb/mdb-setup.sh
@@ -3,34 +3,36 @@
 # Exit immediatly in case of an error
 set -e
 
-/etc/init.d/mysql start
-# or this:
-# systemctl start mysql.service     # maybe??
+if [ ! -d /var/lib/mysql/mysql ]; then
+    /etc/init.d/mysql start
+    # or this:
+    # systemctl start mysql.service     # maybe??
 
-# Initialize the data directory and create the system tables needed to manage databases
-mysql_install_db --user=mysql --datadir='/var/lib/mysql'
+    # Initialize the data directory and create the system tables needed to manage databases
+    mysql_install_db --user=mysql --datadir='/var/lib/mysql/mysql'
 
-# Set a password for the admin aka root
-mysqladmin -u root password $MDB_ROOT_PASS
+    # Set a password for the admin aka root
+    # mysqladmin -u root password $MDB_ROOT_PASS
 
-# Grant all permissions related to all DBs to the root user
-mysql -y root -p"$MDB_ROOT_PASS" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
+    # Grant all permissions related to all DBs to the root user
+    # mysql -u root -p"$MDB_ROOT_PASS" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
 
-# Create a new mariadb database
-mysql -u root -p"$MDB_ROOT_PASS" -e "CREATE DATABASE $WP_DB_NAME;"
+    # Create a new mariadb database
+    mysql -u root -e "CREATE DATABASE $WP_DB_NAME;"
 
-# Create a new mariadb user
-mysql -u root -p"$MDB_ROOT_PASS" -e "CREATE USER '$WP_USER_NAME'@'wordpress' IDENTIFIED BY '$WP_USER_PASS';"
+    # Create a new mariadb user
+    mysql -u root -e "CREATE USER '$WP_USER_NAME'@'wordpress' IDENTIFIED BY '$WP_USER_PASS';"
 
-# Grant all permissions related to the newly created db to the newly created used
-mysql -u root -p"$MDB_ROOT_PASS" -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_USER_NAME'@'wordpress';"
+    # Grant all permissions related to the newly created db to the newly created used
+    mysql -u root -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_USER_NAME'@'wordpress';"
 
-# Clears the in-memory cache of user and privilege information and reloads it from disk
-mysql -u root -p"$MDB_ROOT_PASS" -e "FLUSH PRIVILEGES;"
+    # Clears the in-memory cache of user and privilege information and reloads it from disk
+    mysql -u root -e "FLUSH PRIVILEGES;"
 
-/etc/init.d/mysql stop
-# or this:
-# systemctl stop mysql.service      # maybe??
+    /etc/init.d/mysql stop
+    # or this:
+    # systemctl stop mysql.service      # maybe??
+fi
 
 # Start the MariaDB service
 exec mysqld_safe --skip-syslog --datadir='/var/lib/mysql' --bind-address=0.0.0.0
--- Inception/srcs/requirements/wordpress/wp-cli.sh
+++ Inception/srcs/requirements/wordpress/wp-cli.sh
@@ -3,41 +3,42 @@
 # Exit immediatly in case of an error
 set -e
 
-# Install WP CLI in the Docker container
-curl -O --insecure https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-wp core update
+if [ ! -d /var/www/wordpress ]; then
+    mkdir -p /var/www/wordpress
+    wp core download --allow-root \
+        --path=/var/www/wordpress \
+        --locale=en_US \
+        --version=6.1.1
+    chown -R www-data:www-data /var/www/wordpress
 
-# Download and install WordPress with WP CLI
-wp core download --allow-root \
-    --path=/var/www/wordpress \
-    --locale=en_US \
-    --version=6.1.1
-chown -R www-data:www-data /var/www/wordpress
+    # Command that creates a wp-config.php file
+    wp core config --allow-root \
+        --path=/var/www/wordpress \
+        --dbname=$WP_DB_NAME \
+        --dbuser=$WP_DB_USER \
+        --dbpass=$WP_DB_PASS \
+        --dbhost=$WP_DB_HOST:3306 \
+        --dbprefix=wp_
 
-# Command that creates a wp-config.php file
-wp core config --allow-root \
-    --path=/var/www/wordpress \
-    --dbname=$WP_DB_NAME \
-    --dbuser=$WP_DB_USER \
-    --dbpass=$WP_DB_PASS \
-    --dbhost=$WP_DB_HOST \
-    --dbprefix=wp_
+    # Command which installs WordPress while creating an admin for it
+    wp core install --allow-root \
+        --path=/var/www/wordpress \
+        --url=$DOMAIN_NAME \
+        --title="My Website" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASS \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --skip-email
 
-# Command which installs WordPress while creating an admin for it
-wp core install --allow-root \
-    --path=/var/www/wordpress \
-    --url=$DOMAIN_NAME \
-    --title="My Website" \
-    --admin_user=$WP_ADMIN_USER \
-    --admin_password=$WP_ADMIN_PASS \
-    --admin_email=$WP_ADMIN_EMAIL \
-    --skip-email
+    # Command that creates a user
+    wp user create --allow-root \
+        $WP_USER_NAME \
+        $WP_USER_EMAIL \
+        --role=author \
+        --user_pass=$WP_USER_PASS
+    
+fi
 
-# Command that creates a user
-wp user create --allow-root \
-    $WP_USER_NAME \
-    $WP_USER_EMAIL \
-    --role=author \
-    --user_pass=$WP_USER_PASS
+/usr/sbin/php-fpm7.3 -F -O
+#php-fpm -F -O
+
