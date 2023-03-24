3c0fc6af126fdca51fd1da4a273e6e2dbd825c5b
Mohamed amine Naimi <mnaimi@student.1337.ma>
2023-03-24 08:08:27 +0000

    Added the redis caching service, working properly for now

--- Inception/srcs/requirements/wordpress/wp-cli.sh
+++ Inception/srcs/requirements/wordpress/wp-cli.sh
@@ -10,14 +10,18 @@ fi
 
 sed -i "s|listen = /run/php/php7.3-fpm.sock|listen = 0.0.0.0:9000|g" /etc/php/7.3/fpm/pool.d/www.conf
 
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
-
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
+    # 
     mkdir -p /var/www/wordpress
     chmod -R 777 /var/www/wordpress
 
+    #
     cd /var/www/wordpress
-    
-    wp core download --allow-root
+
+    # 
+    wp core download --allow-root \
+        --locale=en_US \
+        --version=6.1.1
 
     # Command that creates a wp-config.php file
     wp config create --allow-root \
@@ -28,6 +32,7 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
         --dbprefix=wp_
 
     # Command which installs WordPress while creating an admin for it
+    # --url=mnaimi.42.fr
     wp core install --allow-root \
         --title='My_Website' \
         --url=localhost \
@@ -42,11 +47,19 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
         "$WP_USER_EMAIL" \
         --user_pass="$WP_USER_PASS" \
         --role=author
-    
-    wp config set WP_CACHE true --raw
-    wp config set REDIS_HOST redis
-    wp config set REDIS_PORT 6379
 
+    # 
+    wp plugin install redis-cache --activate --allow-root
+    wp redis enable --allow-root
+    wp config set --type=constant --allow-root WP_REDIS_HOST redis
+    wp config set --type=constant --allow-root WP_REDIS_PORT 6379
+    wp config set --type=constant --allow-root WP_CACHE true --raw 
+    wp config set --type=constant --allow-root WP_CACHE_KEY_SALT $WP_DB_NAME
+    wp config set --type=constant --allow-root WP_REDIS_CLIENT predis
+    wp config set --type=constant --allow-root WP_REDIS_SCHEME tcp
+    wp config set --type=constant --allow-root WP_REDIS_DATABASE 0
+    wp config set --type=constant --allow-root WP_REDIS_PASSWORD ''
+    wp config set --type=constant --allow-root WP_REDIS_READ_TIMEOUT 5
 fi
 
 exec "$@"
